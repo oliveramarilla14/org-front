40dc10df0abdebd082d3245252479fa887f578a8
Oliver <oliver.amarilla@grupoepem.com.py>
2024-06-17 16:51:54 -0400

    add fetcher to test swrConfig

--- src/helpers/testRender.tsx
+++ src/helpers/testRender.tsx
@@ -1,10 +1,11 @@
+import { fetcher } from '@/api/fetcher';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 
 export function testRender(component: JSX.Element) {
   return render(
-    <SWRConfig value={{ provider: () => new Map() }}>
+    <SWRConfig value={{ provider: () => new Map(), fetcher }}>
       <MemoryRouter>{component}</MemoryRouter>
     </SWRConfig>
   );
